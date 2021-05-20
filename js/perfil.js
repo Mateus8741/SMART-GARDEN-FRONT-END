8b5a84a48347a3d0d4110a6fe073fb6cc89607ea
nalberthdionisio <nalberthdionisio@aluno.fapce.edu.br>
2021-05-20 14:28:16 -0300

    commit on master

---
+++ js/perfil.js
@@ -0,0 +1,9 @@
+function menuToggle(){
+    const toggleMenu = document.querySelector('.menu');
+    toggleMenu.classList.toggle('active')
+};
+
+function Toggle(){
+    const toggleMenu = document.getElementById('cog');
+    toggleMenu.classList.toggle('active')
+}
