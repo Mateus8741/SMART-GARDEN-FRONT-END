60dfed239e20cd77dfbb61ab8e4bb96b0b809f2f
Mateus8741 <62652109+Mateus8741@users.noreply.github.com>
2021-05-22 11:40:21 -0300

    commit on master

--- js/perfil.js
+++ js/perfil.js
@@ -6,4 +6,9 @@ function menuToggle(){
 function Toggle(){
     const toggleMenu = document.getElementById('cog');
     toggleMenu.classList.toggle('active')
-}
+};
+
+function Toggle2(){
+    const toggleMenu = document.getElementById('nome');
+    toggleMenu.classList.toggle('active')
+};
