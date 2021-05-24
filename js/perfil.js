b7f85fc1bf7ccdabc2280598d5127a3edb46d5ae
Mateus8741 <62652109+Mateus8741@users.noreply.github.com>
2021-05-24 12:45:57 -0300

    commit on master

--- js/perfil.js
+++ js/perfil.js
@@ -1,14 +1,27 @@
+/* ESCONDER MENU DO PERFIL*/
 function menuToggle(){
     const toggleMenu = document.querySelector('.menu');
     toggleMenu.classList.toggle('active')
 };
 
+/* GIRO ENGRENAGEM PERFIL*/
 function Toggle(){
     const toggleMenu = document.getElementById('cog');
     toggleMenu.classList.toggle('active')
 };
 
+/* BOTOES INFERIORES PERFIL*/
 function Toggle2(){
-    const toggleMenu = document.getElementById('nome');
+    const toggleMenu = document.querySelector('form');
     toggleMenu.classList.toggle('active')
 };
+
+/* FLIP PERFIL*/ 
+function teste(){
+    const toggleMenu = document.querySelector('.backk');
+    toggleMenu.classList.toggle('active')
+}
+function teste2(){
+    const toggleMenu = document.querySelector('.card');
+    toggleMenu.classList.toggle('active')
+}
