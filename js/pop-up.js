7e07d23c50779ca922d65cb1ba607fa6573e59d8
Mateus Tavares <62652109+Mateus8741@users.noreply.github.com>
2021-06-02 15:31:32 -0300

    Delete js directory

--- js/pop-up.js
+++
@@ -1,7 +0,0 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "flex";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
